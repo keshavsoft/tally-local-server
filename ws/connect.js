f9e4eb07db146f23e59e57b22d0805c37ec7a858
keshavsoft <nknnkumar@yahoo.com>
2026-09-18 07:24:33 +0530

    connect.js ws url

--- ws/connect.js
+++ ws/connect.js
@@ -4,6 +4,9 @@ import { handleMessage } from "./handleMessage.js";
 
 export const connect = () => {
 
+    // WS_URL = ws://localhost:8080
+    // WS_URL1 = wss://quotation.keshavsoft.com/ws
+
     const ws = new WebSocket(process.env.WS_URL);
 
     ws.on("open", () => {
